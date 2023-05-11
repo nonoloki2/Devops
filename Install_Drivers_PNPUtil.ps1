14d850440738ac0df8a2b71f1fd18559f02b19b7
nonoloki2 <86685407+nonoloki2@users.noreply.github.com>
2023-05-11 15:41:24 -0300

    Add files via upload

---
+++ Install_Drivers_PNPUtil.ps1
@@ -0,0 +1,2 @@
+﻿Get-ChildItem "C:\drivers\" -Recurse -Filter "*.inf" | 
+ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }
---
+++ Set-NewDriversPath.ps1
@@ -0,0 +1,1 @@
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion -Name DevicePath -Value "%SystemRoot%\inf;C:\Drivers"
