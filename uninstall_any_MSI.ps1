d07a39e8fa48b3649d3a266f5e52f6dc58c59389
nonoloki2 <86685407+nonoloki2@users.noreply.github.com>
2023-05-24 07:28:37 -0300

    Add files via upload

---
+++ uninstall_any_MSI.ps1
@@ -0,0 +1,22 @@
+﻿### List the installed applications.
+
+$INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, UninstallString
+$INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, UninstallString
+$INSTALLED | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | Format-Table -AutoSize
+
+### Search for the MSI application (Remenber change 7-zip by your application).
+
+$SEARCH = '7-zip*'
+$RESULT =$INSTALLED | ?{ $_.DisplayName -ne $null } | Where-Object {$_.DisplayName -match $search } 
+$RESULT
+
+### Uninstall Any MSI Exec Application
+
+if ($RESULT.uninstallstring -like "msiexec*") {
+$ARGS=(($RESULT.UninstallString -split ' ')[1] -replace '/I','/X ') + ' /q'
+Start-Process msiexec.exe -ArgumentList $ARGS -Wait
+} else {
+$UNINSTALL_COMMAND=(($RESULT.UninstallString -split '\"')[1])
+$UNINSTALL_ARGS=(($RESULT.UninstallString -split '\"')[2]) + ' --force-uninstall'
+Start-Process -Filepath $UNINSTALL_COMMAND -ArgumentList $UNINSTALL_ARGS -Wait
+}
