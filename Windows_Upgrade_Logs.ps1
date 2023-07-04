28f7ddcec4ee4ff8f2bcb263c0780d04249ad6fc
nonoloki2 <86685407+nonoloki2@users.noreply.github.com>
2023-07-04 12:26:32 -0300

    Copy Windows Upgrade Logs for network location
    Copy Windows Upgrade Logs for network location with machine hostname (indentifying machine logs owner)

---
+++ Windows_Upgrade_Logs.ps1
@@ -0,0 +1,9 @@
+﻿$hostname = $env:COMPUTERNAME
+$saveLocation = Read-Host "Enter the location to save the zip file"
+
+# Create a new zip file with the machine hostname
+$zipFileName = "$hostname.zip"
+$zipFilePath = Join-Path -Path $saveLocation -ChildPath $zipFileName
+$zip = [System.IO.Compression.ZipFile]::CreateFromDirectory("C:\$WINDOWS.~BT\Sources\Panther\", $zipFilePath)
+
+Write-Host "Log files have been zipped and saved to: $zipFilePath"
