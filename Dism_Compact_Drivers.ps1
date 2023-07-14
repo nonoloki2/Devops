e19e17be04fd36aff0df049e4d810b9240c9584f
nonoloki2 <86685407+nonoloki2@users.noreply.github.com>
2023-07-14 11:59:24 -0300

    Update Dism_Compact_Drivers.ps1

--- Dism_Compact_Drivers.ps1
+++ Dism_Compact_Drivers.ps1
@@ -1,16 +1,9 @@
-# Solicitar o caminho da pasta de origem
-$origem = Read-Host "Digite o caminho da pasta de origem"
+# Prompt for source and destination folders
+$source = Read-Host "Enter the source folder path:"
+$destination = Read-Host "Enter the destination folder path:"
 
-# Solicitar o caminho do arquivo WIM de destino
-$destino = Read-Host "Digite o caminho do arquivo WIM de destino"
+# Build the command
+$command = "dism /Capture-Image /ImageFile:`"$destination\drivers.wim`" /CaptureDir:`"$source`" /Compress:Max /Name:DRIVERS"
 
-# Comprimir a pasta de origem em um arquivo .wim usando o DISM
-$command = "dism /Capture-Image /ImageFile:`"$destino`" /CaptureDir:`"$origem`" /Compress:Max /Name:DRIVERS"
-Start-Process -FilePath "cmd.exe" -ArgumentList "/c $command" -Wait
-
-# Verificar se a captura foi bem-sucedida
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "A pasta foi capturada e comprimida com sucesso em um arquivo .wim."
-} else {
-    Write-Host "Ocorreu um erro ao capturar a pasta e comprimi-la em um arquivo .wim."
-}
+# Execute the command
+Invoke-Expression -Command $command
