682f9f7c98ac89d06f312612de896f47432e52ff
nonoloki2 <86685407+nonoloki2@users.noreply.github.com>
2023-07-14 11:58:25 -0300

    Add files via upload

---
+++ Compact_Drivers_Dism.ps1
@@ -0,0 +1,9 @@
+﻿# Prompt for source and destination folders
+$source = Read-Host "Enter the source folder path:"
+$destination = Read-Host "Enter the destination folder path:"
+
+# Build the command
+$command = "dism /Capture-Image /ImageFile:`"$destination\drivers.wim`" /CaptureDir:`"$source`" /Compress:Max /Name:DRIVERS"
+
+# Execute the command
+Invoke-Expression -Command $command
