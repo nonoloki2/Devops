f216f6423d21c8727ba540c3707ccd294cb8b07a
nonoloki2 <86685407+nonoloki2@users.noreply.github.com>
2023-11-28 09:30:05 -0300

    Add files via upload

---
+++ Check_updates_Last_Hour.ps1
@@ -0,0 +1,28 @@
+﻿# Define the time interval to check (in minutes)
+$interval = 60
+
+# Get the current time
+$currentTime = Get-Date
+
+# Calculate the start time for the check
+$startTime = $currentTime.AddMinutes(-$interval)
+
+# Define the log name for Windows Update events
+$logName = "Microsoft-Windows-WindowsUpdateClient/Operational"
+
+# Query the Windows Update event logs for events in the specified interval
+$windowsUpdateEvents = Get-WinEvent -LogName $logName | Where-Object {
+    $_.TimeCreated -ge $startTime -and ($_.Id -eq 19 -or $_.Id -eq 25)
+}
+
+# Check if any events were found
+if ($windowsUpdateEvents.Count -gt 0) {
+    Write-Host "Windows Update ran in the last $interval minutes."
+    $result = $true
+} else {
+    
+    $result = $false
+}
+
+# Return the result
+Exit $result
