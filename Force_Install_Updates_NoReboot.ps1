ba56d325c4b0256657330073f674ae194c83798d
nonoloki2 <86685407+nonoloki2@users.noreply.github.com>
2023-11-28 09:30:44 -0300

    Add files via upload

---
+++ Force_Install_Updates_NoReboot.ps1
@@ -0,0 +1,31 @@
+﻿# Install PSWindowsUpdate module (if not already installed)
+if (-not (Get-Module -Name PSWindowsUpdate -ListAvailable)) {
+    Install-Module -Name PSWindowsUpdate -Force -AllowClobber
+}
+
+ 
+
+# Import PSWindowsUpdate module
+Import-Module PSWindowsUpdate
+
+ 
+
+# Force check for updates
+(New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()
+
+ 
+
+# Get a list of pending updates
+$updates = Get-WUList
+
+ 
+
+# Install pending updates without rebooting
+$updatesToInstall = $updates | Where-Object { $_.IsInstalled -eq $false }
+$session = New-WUSession -Confirm:$false
+Install-WUUpdates -Session $session -Updates $updatesToInstall
+
+ 
+
+# Close the update session
+$session.Dispose()
---
+++ Force_WinUpdatesOption2.ps1
@@ -0,0 +1,10 @@
+﻿Install-PackageProvider NuGet -Force
+
+If(-not(Get-InstalledModule PSWindowsUpdate -ErrorAction silentlycontinue))
+{
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module PSWindowsUpdate -Confirm:$False -Force
+
+}
+
+Install-WindowsUpdate -MicrosoftUpdate -NotCategory "Drivers","FeaturePacks" -AcceptAll -IgnoreReboot -Verbose
